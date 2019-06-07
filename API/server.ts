e42f7138f15f95988e7c4b043318ec464fecef48
Rakesh Sinha <rakeshsinha31@gmail.com>
2019-06-07 17:26:27 +0800

    Change callbacks to promises

--- API/server.ts
+++ API/server.ts
@@ -1,14 +1,14 @@
 import { GraphQLServer } from "graphql-yoga";
-const amqp = require("amqplib/callback_api");
+import { connect } from "amqplib";
 
 const typeDefs = `
   type Query {
-    listAccounts: String!
+    listAccounts: String
   }`;
 
 const resolvers = {
   Query: {
-    listAccounts: () => rpc_client("listAccounts")
+    listAccounts: () => rpcClient("listAccounts")
   }
 };
 const server = new GraphQLServer({
@@ -17,51 +17,42 @@ const server = new GraphQLServer({
 });
 server.start(() => console.log("Server is  running on localhost:4000"));
 
-function rpc_client(listAccounts: any): any {
-  amqp.connect("amqp://localhost", function(error0: any, connection: any) {
-    if (error0) {
-      throw error0;
-    }
-    connection.createChannel(function(error1: any, channel: any) {
-      if (error1) {
-        throw error1;
-      }
-      channel.assertQueue(
-        "",
-        {
-          exclusive: true
-        },
-        function(error2: any, q: any) {
-          if (error2) {
-            throw error2;
-          }
-          var correlationId = generateUuid();
-          console.log(" [x] Requesting ", listAccounts);
-          channel.consume(
-            q.queue,
-            function(msg: any) {
-              console.log(msg);
-              if (msg.properties.correlationId == correlationId) {
-                console.log(" [.] Got %s", msg.content.toString());
-                setTimeout(function() {
-                  connection.close();
-                  process.exit(0);
-                }, 500);
-              }
-            },
-            {
-              noAck: true
-            }
-          );
-          channel.sendToQueue("rpc_queue", Buffer.from(listAccounts), {
-            correlationId: correlationId,
-            replyTo: q.queue
-          });
+async function rpcClient(listAccounts: any): Promise<any> {
+  const connection = await connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  const createdQeue = await channel.assertQueue("", { exclusive: true });
+  const correlationId = await generateUuid();
+  console.log(" [x] Requesting ", listAccounts);
+
+  function consume(channel: any, createdQeue: any): Promise<any> {
+    console.log("inside consume...");
+    return new Promise(resolve => {
+      channel.consume(createdQeue.queue, async function(msg: any) {
+        let msgBody = msg.content.toString();
+
+        console.log("inside promise...");
+        console.log(msgBody);
+
+        if (true) {
+          console.log(msg);
+          resolve(JSON.parse(msgBody));
         }
-      );
+        await channel.ack(msg);
+      });
     });
-  });
+  }
+  // consume(channel, createdQeue)
+  //   .then(() => console.log("THEN"))
+  //   .catch(() => console.log("CATCH"));
+
+  consume(channel, createdQeue).then(() =>
+    channel.sendToQueue("rpc_queue", Buffer.from(listAccounts), {
+      correlationId: correlationId,
+      replyTo: createdQeue.queue
+    })
+  );
 }
+
 function generateUuid() {
   return (
     Math.random().toString() +
